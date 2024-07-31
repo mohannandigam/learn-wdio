8e4d7ad965e02c3bfd2ace4388df371cce438e72
Mohan Nandigam <mohan.nandigam@ssense.com>
2024-07-30 23:46:59 -0400

    update-2024july30

--- test/specs/loggerTest.ts
+++
@@ -1,9 +0,0 @@
-import { logger } from '../../shared/logger';
-import LoginPage from '../pageobjects/login.page'
-
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
-        logger.info('Info message');
-    })
-})
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -134,7 +134,7 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
+    reporters: [['allure', {outputDir: 'allure-results'}]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
