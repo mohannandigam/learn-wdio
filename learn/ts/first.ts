12b2e621ed96bd6df5883e7845ff283cbad6cd35
Mohan Kishna Nandigam <nmkgangadhar@gmail.com>
2024-08-11 18:05:11 -0400

    11Aug-2024

---
+++ learn/ts/first.ts
@@ -0,0 +1,11 @@
+console.log("Hello World");
+let mk;
+mk = 10;
+console.log(mk);
+mk = "Hello";
+console.log(mk);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+console.log(add(10, 20));
